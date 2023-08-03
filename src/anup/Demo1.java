656affdf19c42d7010d13945e78207871e1dd77e
SHALIKRAM MAHAJAN <anupmahajan2707@gmail.com>
2023-08-03 10:50:24 +0530

    first commit

---
+++ src/anup/Demo1.java
@@ -0,0 +1,21 @@
+package anup;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://demowebshop.tricentis.com/");
+		
+		
+
+}
+}
